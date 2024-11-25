5a66c85cd470c50d811733a5a9373b35012aca7c
Gabriel Igor Ferreira Martins <92898420+IgorFMG@users.noreply.github.com>
2024-11-24 21:02:19 -0300

    Add files via upload

---
+++ OatEstruturasCondicionais.py
@@ -0,0 +1,29 @@
+def calc_reajust(sal_atual):
+    inf = 3.8 / 100
+
+    if sal_atual <= 280:
+        percentual = 20
+    elif sal_atual <= 700:
+        percentual = 15
+    elif sal_atual <= 1500:
+        percentual = 10
+    else:
+        percentual = 5
+
+    valor = sal_atual * (percentual/ 100)
+    nov_sal = sal_atual+ valor
+    aumento_valor = valor * (1 - inf)
+
+
+    print(f"Salario antes do reajuste é igual: R$ {sal_atual:.2f}")
+    print('---------------------------------------------------')
+    print(f"Percentual de aumento aplicado: {percentual}%")
+    print('---------------------------------------------------')
+    print(f"Valor adicional: R$ {valor:.2f}")
+    print('---------------------------------------------------')
+    print(f"Novo salario , apos o aumento: R$ {nov_sal:.2f}")
+    print('---------------------------------------------------')
+    print(f"Valor depois do reajuste, descontado a inflação: R$ {aumento_valor:.2f}")
+
+    salario_atual = float(input("Digite o salário atual do colaborador: "))
+    calc_reajust(salario_atual)
